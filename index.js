ccafa8e5b6f94628c5e7f5952977ccb4458bd857
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2022-06-16 17:13:32 -0600

    commit tree e88b0afa5db0593b78b03e03f33d09b77eb04937

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9 '-]/gi, '');
+  }
+
+  static titleize(string) {
+    let array = [];
+    let excludeWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    array = string.split(' ');
+    return array.map((word, i) => {
+      return excludeWords.includes(word) && i!=0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    }) .join(' ');
+  }
 }
